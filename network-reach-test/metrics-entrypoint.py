421120c687c49615f7c5a477b2db1fad7cb7d0cc
Tonia-Elengikal <tonia.elengikal@ibm.com>
2023-04-19 12:32:10 -0400

    adding Python code to check Multi-NIC CNI health checker metrics and new Dockerfile

---
+++ network-reach-test/metrics-entrypoint.py
@@ -0,0 +1,50 @@
+from kubernetes import client
+from kubernetes.client.rest import ApiException
+import os
+from datetime import datetime
+import subprocess
+
+
+
+def main():
+   
+    nodename = os.getenv("NODE_NAME")
+    namespace = os.getenv("NAMESPACE")
+    command = ['python', 'read_status.py', nodename]
+    result = subprocess.run(command, capture_output=True, text=True)
+    output = result.stdout
+    print(output)
+    
+    if "OK" in output:
+        print("Node " + nodename + " is reachable")
+    else:
+        alert = "Node " + nodename + " is not reachable"
+        print(alert)
+
+        api = client.CustomObjectsApi()
+
+        dt = datetime.now()
+        hcr_manifest = {
+            'apiVersion': 'my.domain/v1alpha1',
+            'kind': 'HealthCheckReport',
+            'metadata': {
+                'name': "netcheck-"+nodename+"-"+dt.strftime("%d-%m-%Y-%H.%M.%S.%f")
+            },
+            'spec': {
+                'node': nodename,
+                'report': alert,
+                'issuer': "net-reach"
+            }
+        }
+        group = "my.domain"
+        v = "v1alpha1"
+        plural = "healthcheckreports"
+        try:
+            api.create_namespaced_custom_object(group, v, namespace, plural, hcr_manifest)
+        except ApiException as e:
+            print("Exception when calling create health check report:\n", e)
+
+        raise TypeError("Failing init container.")
+
+if __name__ == '__main__':
+    main()
