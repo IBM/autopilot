4dc019a42bae3aeb1034dedf66a103fa58372921
Claudia <c.misale@ibm.com>
2023-05-17 15:46:16 -0400

    minor updates and begin with exposing prometheus metrics

--- autopilot-daemon/pkg/utils/global.go
+++ autopilot-daemon/pkg/utils/global.go
@@ -14,15 +14,23 @@ type InitConfig struct {
 var UserConfig InitConfig
 
 var (
-	Mutations = prometheus.NewCounter(
+	Requests = prometheus.NewCounter(
 		prometheus.CounterOpts{
 			Name: "healthcheck_req_total",
 			Help: "Number of invocations to Autopilot",
 		},
 	)
+
+	Hchecks = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name: "health_report_total",
+			Help: "Summary of the health checks measurements on compute nodes.",
+		},
+		[]string{"health", "node", "deviceid"},
+	)
 )
 
 func Initmetrics(reg prometheus.Registerer) {
 	// Register custom metrics with the global prometheus registry
-	reg.MustRegister(Mutations)
+	reg.MustRegister(Requests, Hchecks)
 }
