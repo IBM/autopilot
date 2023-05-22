c70a4fa97f9f8bf01f3ad14b3b5c2b2575305d85
Claudia <c.misale@ibm.com>
2023-05-22 11:50:43 -0400

    Gauge and Summary metrics

--- autopilot-daemon/pkg/handlers/handler.go
+++ autopilot-daemon/pkg/handlers/handler.go
@@ -3,8 +3,12 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 
+	"github.ibm.com/hybrid-cloud-infrastructure-research/autopilot-daemon/pkg/utils"
 	"k8s.io/klog/v2"
 )
 
@@ -16,10 +20,25 @@ func PCIeBWHandler(pciebw string) http.Handler {
 			klog.Error(err.Error())
 		} else {
 			klog.Info("GPU PCIe BW test completed:")
-			output := string(out[:])
-			fmt.Println(output)
+			w.Write(out)
+			output := strings.TrimSuffix(string(out[:]), "\n")
+
+			split := strings.Split(output, "\n")
+			bws := split[len(split)-1]
+			final := strings.Split(bws, " ")
+
+			for gpuid, v := range final {
+				bw, err := strconv.ParseFloat(v, 64)
+				if err != nil {
+					klog.Error(err.Error())
+				} else {
+					klog.Info("Observation: ", os.Getenv("NODE_NAME"), " ", strconv.Itoa(gpuid), " ", bw)
+					utils.Hchecks.WithLabelValues("pciebw", os.Getenv("NODE_NAME"), strconv.Itoa(gpuid)).Observe(bw)
+					utils.HchecksGauge.WithLabelValues("pciebw", os.Getenv("NODE_NAME"), strconv.Itoa(gpuid)).Set(bw)
+				}
+			}
 		}
-		w.Write(out)
+
 	}
 	return http.HandlerFunc(fn)
 }
@@ -34,6 +53,8 @@ func GPUMemHandler() http.Handler {
 func NetReachHandler() http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Requesting secondary nics reachability test\n"))
+		// utils.Hchecks.WithLabelValues("netdevice", "worker-2", "1").Observe(1.0)
+		// utils.Hchecks.WithLabelValues("netdevice", "worker-2", "0").Observe(1.0)
 		out, err := exec.Command("python3", "./network/metrics-entrypoint.py").Output()
 		if err != nil {
 			klog.Error(err.Error())
