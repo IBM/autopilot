1ad29f16545415d754c477a97fb19255230dd5c7
eletonia <elentonia@gmail.com>
2023-05-25 16:15:42 -0400

    adding line of output to net reach test

--- network-reach-test/metrics-entrypoint.py
+++ network-reach-test/metrics-entrypoint.py
@@ -26,29 +26,42 @@ def main():
     else:
         alert = "Node " + nodename + " is not reachable"
         print(alert)
+    
+    connectable = output.split("Connectable network devices: ")[1]
+    devices = int(connectable.split("/")[0])
+    if devices == 2:
+        lastline = nodename + " 1 1"
+    elif devices == 1:
+        lastline = nodename + " 1 0"
+    elif devices == 0:
+        lastline = nodename + " 0 0"
+    else:
+        lastline = "Cannot determine connectable devices"
+    
+    print("\n" + lastline)
 
-        api = client.CustomObjectsApi()
+        # api = client.CustomObjectsApi()
 
-        dt = datetime.now()
-        hcr_manifest = {
-            'apiVersion': 'my.domain/v1alpha1',
-            'kind': 'HealthCheckReport',
-            'metadata': {
-                'name': "netcheck-"+nodename+"-"+dt.strftime("%d-%m-%Y-%H.%M.%S.%f")
-            },
-            'spec': {
-                'node': nodename,
-                'report': alert,
-                'issuer': "net-reach"
-            }
-        }
-        group = "my.domain"
-        v = "v1alpha1"
-        plural = "healthcheckreports"
-        try:
-            api.create_namespaced_custom_object(group, v, namespace, plural, hcr_manifest)
-        except ApiException as e:
-            print("Exception when calling create health check report:\n", e)
+        # dt = datetime.now()
+        # hcr_manifest = {
+        #     'apiVersion': 'my.domain/v1alpha1',
+        #     'kind': 'HealthCheckReport',
+        #     'metadata': {
+        #         'name': "netcheck-"+nodename+"-"+dt.strftime("%d-%m-%Y-%H.%M.%S.%f")
+        #     },
+        #     'spec': {
+        #         'node': nodename,
+        #         'report': alert,
+        #         'issuer': "net-reach"
+        #     }
+        # }
+        # group = "my.domain"
+        # v = "v1alpha1"
+        # plural = "healthcheckreports"
+        # try:
+        #     api.create_namespaced_custom_object(group, v, namespace, plural, hcr_manifest)
+        # except ApiException as e:
+        #     print("Exception when calling create health check report:\n", e)  
 
 if __name__ == '__main__':
     main()
