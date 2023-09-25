568171a0c8a65b9ae31c6a2f08ef32eae3ae398f
Claudia Misale <c.misale@ibm.com>
2023-09-24 16:31:04 -0900

    Select source node to run iperf client

--- autopilot-daemon/network/iperf3-debrief.sh
+++ autopilot-daemon/network/iperf3-debrief.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo Unreachable servers, if any:
 for FILE in out*; do 
     unable=`grep unable ${FILE}`
     if [ ! -z "$unable" ]
@@ -13,7 +14,6 @@ cat out* | grep receiver| awk '{print $7}'| awk '{s+=$1}END{print s}'
 echo Aggregate bw per interface
 for i in $(ls out*); do echo $(echo ${i}| cut -d ':' -f 2) "${i##*_}" $(cat ${i} | grep receiver| awk '{print $7}'| awk '{s+=$1}END{print s}');done 
 
-echo Unreachable servers printed above, if any
 count=$(cat out* | grep unable | wc -l)
 echo Unreachable servers count: $count
 
