1456dd5725c788830837f49fcf4a0542fbd5ca81
Claudia <c.misale@ibm.com>
2023-10-16 10:09:52 -0400

    file restore and image tag

--- autopilot-daemon/gpu-remapped/entrypoint.py
+++ autopilot-daemon/gpu-remapped/entrypoint.py
@@ -2,25 +2,25 @@ import os
 
 
 def main():
-    output = os.popen('bash ./gpu-power/briefings.sh')
+    output = os.popen('bash ./gpu-remapped/briefings.sh')
     result = output.read()
     print(result)
 
     if "ABORT" not in result:
-        print("[[ POWER THROTTLE ]] Briefings completed. Continue with power throttle evaluation.")
-        output = os.popen('./gpu-power/power-throttle.sh')
+        print("[[ REMAPPED ROWS ]] Briefings completed. Continue with remapped rows evaluation.")
+        output = os.popen('./gpu-remapped/remapped-rows.sh')
         result = output.read()
         if "FAIL" not in result:
-            print("[[ POWER THROTTLE ]] SUCCESS")
+            print("[[ REMAPPED ROWS ]] SUCCESS")
             # print(result)
         else:
-            print("[[ POWER THROTTLE ]] FAIL")
+            print("[[ REMAPPED ROWS ]] FAIL")
             print("Host ", os.getenv("NODE_NAME"))
             return 0
         print("Host ", os.getenv("NODE_NAME"))
         print(result.strip())
     else:
-        print("[[ POWER THROTTLE ]] ABORT")
+        print("[[ REMAPPED ROWS ]] ABORT")
         print(result)
 
 if __name__ == '__main__':
