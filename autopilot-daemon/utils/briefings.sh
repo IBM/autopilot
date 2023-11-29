8ad4c73889953294a585e6e3f77abdc222c87821
Claudia <c.misale@ibm.com>
2023-11-29 08:47:09 -0500

    code cleaning and refactoring

---
+++ autopilot-daemon/utils/briefings.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+exists=`which nvidia-smi`
+if [[ -z $exists ]]
+then
+	echo !! nvidia-smi not present. ABORT.
+	exit 
+fi
+
+CMD="$(nvidia-smi)"
+errors="$(echo ${CMD} | grep ERR)"
+if [[ -n $errors ]]
+then
+	echo !! nvidia-smi failed to start. ABORT.
+	exit 
+fi
+
+CMD="$(nvidia-smi --query-gpu=mig.mode.current --format=csv)"
+mig="$(echo ${CMD} | grep Enabled)"
+if [[ -n $mig ]]
+then
+	echo !! MIG enabled. ABORT.
+	exit 
+fi
+
+CMD="$(dcgmi --version)"
+errors="$(echo ${CMD} | grep -i 'fail|error')"
+if [[ -n $errors ]]
+then
+	echo !! dcgmi failed to start. ABORT.
+	exit 
+fi
