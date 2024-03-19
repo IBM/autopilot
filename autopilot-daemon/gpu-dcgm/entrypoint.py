18b80870f120fd365d0c23a5be19c5a6273f844b
Claudia Misale <34547329+cmisale@users.noreply.github.com>
2024-03-19 13:47:25 -0400

    First Invasive check through external K8s Job - dcgmi (#8)
    * update to golang version
    
    Signed-off-by: Claudia <c.misale@ibm.com>
    
    * add singleton clientset instance
    
    Signed-off-by: Claudia <c.misale@ibm.com>
    
    * changes to dependencies
    
    Signed-off-by: Claudia <c.misale@ibm.com>
    
    * intrusive checks enablement
    
    Signed-off-by: Claudia <c.misale@ibm.com>
    
    * This is a combination of 3 commits.
    
    first take on node labeling on dcgm 3 failure
    
    first take on node labeling on dcgm 3 failure
    
    explicit set of resources, plus some minor stuff
    
    Signed-off-by: Claudia <c.misale@ibm.com>
    
    * bugfix: better error handling
    
    Signed-off-by: Claudia <c.misale@ibm.com>
    
    * bugfix: unreachable node count
    
    Signed-off-by: Claudia <c.misale@ibm.com>
    
    * Minor touches. Set invasive timer to 0 to avoid them
    
    Signed-off-by: Claudia <c.misale@ibm.com>
    
    * Update functions.go
    
    Removed comments
    
    ---------
    
    Signed-off-by: Claudia <c.misale@ibm.com>

--- autopilot-daemon/gpu-dcgm/entrypoint.py
+++ autopilot-daemon/gpu-dcgm/entrypoint.py
@@ -2,12 +2,19 @@ import json
 import subprocess
 import os
 import argparse
+import datetime
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 
+config.load_incluster_config()
+v1 = client.CoreV1Api()
+nodename = os.getenv("NODE_NAME")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', '--run', type=str, default='1')
+parser.add_argument('-l', '--label_node', action='store_true')
+args = parser.parse_args()
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--run', type=str, default='1')
-    args = parser.parse_args()
-
     output = os.popen('bash ./utils/briefings.sh')
     result = output.read()
     print(result)
@@ -21,9 +28,9 @@ def main():
         print(result)
 
 def try_dcgm(command):
-    try:
-        result = subprocess.run(command, check=True, text=True, capture_output=True)
-    except subprocess.CalledProcessError:
+    result = subprocess.run(command, text=True, capture_output=True)
+    return_code = result.returncode  # 0 for success
+    if return_code != 0:
         print("[[ DCGM ]] DCGM process terminated with errors. Other processes might be running on GPUs. ABORT")
         command = ['nvidia-smi', '--query-gpu=utilization.gpu', '--format=csv']
         try:
@@ -33,12 +40,10 @@ def try_dcgm(command):
             exit()
         if proc.stdout:
             print("[[ DCGM ]] GPUs currently utilized:\n", proc.stdout)
-        exit()
-
+    
     if result.stderr:
        print(result.stderr)
        print("[[ DCGM ]] exited with error: " + result.stderr + " ERR")
-       exit()
     else:
         dcgm_dict = json.loads(result.stdout)
         tests_dict = dcgm_dict['DCGM GPU Diagnostic']['test_categories']
@@ -47,15 +52,43 @@ def try_dcgm(command):
         for category in tests_dict:
             for test in category['tests']:
                 if test['results'][0]['status'] == 'Fail':
-                    print(test['name'], ":", test['results'][0]['status'])
                     success = False
-                    output+=(test['name']+" ")
+                    print(test['name'], ":", test['results'][0]['status'])
+                    if test['name'] == "GPU Memory":
+                        output+=(test['name'].replace(" ","")+"_")
+                        for entry in test['results']:
+                            output+=("."+entry['gpu_id'])
         if success:
             print("[[ DCGM ]] SUCCESS")
         else:
-            print("Host ", os.getenv("NODE_NAME"))
+            print("Host", nodename)
             print("[[ DCGM ]] FAIL")
-            print(output.strip())
+        if args.label_node:
+            patch_node(success, output)
+    
+
+def patch_node(success, output):
+    now = datetime.datetime.now(datetime.timezone.utc)
+    #  ADD UTC
+    timestamp = now.strftime("%Y-%m-%d_%H.%M.%SUTC")
+    result = ""
+    if success:
+        result = "PASS_"+timestamp
+    else:
+        result = "ERR_"+timestamp+"_"+output
+
+    label = {
+        "metadata": {
+            "labels": {
+                "autopilot/dcgm.level.3": result}
+        }
+    }
+    print("label: ", result)
+    try:
+        api_response = v1.patch_node(nodename, label)
+    except ApiException as e:
+        print("Exception when calling corev1api->patch_node: %s\n" % e)
+        exit()
 
 if __name__ == '__main__':
     main()
---
+++ autopilot-daemon/pkg/utils/functions.go
@@ -0,0 +1,147 @@
+package utils
+
+import (
+	"os"
+
+	"context"
+
+	"github.com/thanhpk/randstr"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2"
+	resourcehelper "k8s.io/kubectl/pkg/util/resource"
+)
+
+func GetClientsetInstance() *K8sClientset {
+	if k8sClientset == nil {
+		csetLock.Lock()
+		defer csetLock.Unlock()
+		if k8sClientset == nil {
+			k8sClientset = &K8sClientset{}
+			config, err := rest.InClusterConfig()
+			if err != nil {
+				panic(err.Error())
+			}
+			k8sClientset.Cset, err = kubernetes.NewForConfig(config)
+			if err != nil {
+				panic(err.Error())
+			}
+		}
+
+	}
+	return k8sClientset
+}
+
+// Returns true if GPUs are not currently requested by any workload
+func GPUsAvailability() bool {
+	cset := GetClientsetInstance()
+
+	fieldselector, err := fields.ParseSelector("spec.nodeName=" + os.Getenv("NODE_NAME") + ",status.phase!=" + string(corev1.PodSucceeded))
+	if err != nil {
+		klog.Info("Error in creating the field selector", err.Error())
+		return false
+	}
+	pods, err := cset.Cset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{
+		FieldSelector: fieldselector.String(),
+	})
+	if err != nil {
+		klog.Info("Cannot list pods:", err.Error())
+		return false
+	}
+	for _, pod := range pods.Items {
+		podReqs, podLimits := resourcehelper.PodRequestsAndLimits(&pod)
+		gpuReq := podReqs["nvidia.com/gpu"]
+		gpuLim := podLimits["nvidia.com/gpu"]
+		if gpuReq.Value() > 0 || gpuLim.Value() > 0 {
+			klog.Info("Pod ", pod.Name, " with requests ", gpuReq.Value(), " and limits ", gpuLim.Value(), ". Cannot run invasive health checks.")
+			return false
+		}
+	}
+	klog.Info("GPUs are free. Can run invasive health checks.")
+	return true
+}
+
+func CreateJob(healthcheck string) {
+	var args []string
+	var cmd []string
+	switch healthcheck {
+	case "dcgm":
+		cmd = []string{"python3"}
+		args = []string{"gpu-dcgm/entrypoint.py", "-r", "3", "-l"}
+	}
+	cset := GetClientsetInstance()
+
+	fieldselector, err := fields.ParseSelector("metadata.name=" + os.Getenv("POD_NAME"))
+	if err != nil {
+		klog.Info("Error in creating the field selector", err.Error())
+	}
+	pods, err := cset.Cset.CoreV1().Pods("autopilot").List(context.TODO(), metav1.ListOptions{
+		FieldSelector: fieldselector.String(),
+	})
+	if err != nil {
+		klog.Info("Cannot get pod:", err.Error())
+	}
+	autopilotPod := pods.Items[0]
+	ttlsec := int32(4 * 60 * 60) // setting TTL to 4 hours
+	backofflimits := int32(0)
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      healthcheck + "-" + randstr.Hex(6),
+			Namespace: autopilotPod.Namespace,
+		},
+		Spec: batchv1.JobSpec{
+			TTLSecondsAfterFinished: &ttlsec,
+			BackoffLimit:            &backofflimits,
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					RestartPolicy:      "Never",
+					ServiceAccountName: "autopilot",
+					NodeName:           os.Getenv("NODE_NAME"),
+					InitContainers: []corev1.Container{
+						{
+							Name:            "init",
+							Image:           autopilotPod.Spec.InitContainers[0].DeepCopy().Image,
+							ImagePullPolicy: "IfNotPresent",
+							Command:         autopilotPod.Spec.InitContainers[0].DeepCopy().Command,
+							Args:            autopilotPod.Spec.InitContainers[0].DeepCopy().Args,
+						},
+					},
+					Containers: []corev1.Container{
+						{
+							Name:            "main",
+							Image:           autopilotPod.Spec.Containers[0].DeepCopy().Image,
+							ImagePullPolicy: "IfNotPresent",
+							Command:         cmd,
+							Args:            args,
+							Resources: corev1.ResourceRequirements{
+								Limits: corev1.ResourceList{
+									"nvidia.com/gpu": resource.MustParse("8"),
+								},
+								Requests: corev1.ResourceList{
+									"nvidia.com/gpu": resource.MustParse("8"),
+								},
+							},
+							Env: []corev1.EnvVar{
+								{
+									Name:  "NODE_NAME",
+									Value: os.Getenv("NODE_NAME"),
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	klog.Info("Try create Job")
+	_, err = cset.Cset.BatchV1().Jobs("autopilot").Create(context.TODO(), job,
+		metav1.CreateOptions{})
+	if err != nil {
+		klog.Info("Couldn't create Job ", err.Error())
+	}
+}
