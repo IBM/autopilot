4f6f47993a27065cd8b5f3c4393c4f08bbcfece1
anishs <anish2sinha@gmail.com>
2024-09-22 21:48:43 -0400

    added basic layout of Login, Monitor, and Testing pages with sidebar

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Monitor from './Monitor';
+import Testing from './Testing';
+import Sidebar from './components/Sidebar';
+
 function App() {
   return (
-    <div>
-      <h1>Autopilot Dashboard</h1>
-    </div>
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <Sidebar />
+        <div style={{ marginLeft: '220px', padding: '20px' }}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/monitor" element={<Monitor />} />
+            <Route path="/testing" element={<Testing />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ app/src/Testing.jsx
@@ -0,0 +1,9 @@
+function Testing() {
+    return (
+        <div>
+            <h1>Run Tests</h1>
+        </div>
+    );
+}
+
+export default Testing;
