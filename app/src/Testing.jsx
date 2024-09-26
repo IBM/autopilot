c64a407d867e7ea3dedb68e4d4264422e692122c
anishs <anish2sinha@gmail.com>
2024-09-26 14:03:15 -0400

    added standardized Button component and included this button in Testing Page for 'Select All' and 'Run Tests'

--- app/src/Testing.jsx
+++ app/src/Testing.jsx
@@ -1,7 +1,31 @@
+import React from 'react';
+import Button from './components/Button';
+
 function Testing() {
+
+    const runTests = () => {
+        console.log('Run Tests clicked');
+    };
+
+    const selectAllNodes = () => {
+        console.log('Select All clicked');
+    };
+
     return (
         <div>
             <h1>Run Tests</h1>
+
+            <Button
+                text="Select All"
+                color="green"
+                onClick={selectAllNodes}
+            />
+
+            <Button
+                text="Run Tests"
+                color="blue"
+                onClick={runTests}
+            />
         </div>
     );
 }
