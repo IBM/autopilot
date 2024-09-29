6f50b9a68d958af2227dc9985c882fe206f1ed88
anishs <anish2sinha@gmail.com>
2024-09-29 16:57:25 -0400

    installed mui, created multiselect component and incorporated for tests selection in tests page

--- app/src/Testing.jsx
+++ app/src/Testing.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './components/Button';
+import MultiSelect from './components/MultiSelect';
 
 function Testing() {
 
+    const [selectedTests, setSelectedTests] = useState([]);
+    const tests = ['Test 1', 'Test 2', 'Test 3', 'Test 4'];
+
+    const handleMultiSelectChange = (selected) => {
+        setSelectedTests(selected);
+    };
+
     const runTests = () => {
         console.log('Run Tests clicked');
+        console.log(selectedTests)
     };
 
     const selectAllNodes = () => {
@@ -25,6 +34,13 @@ function Testing() {
                 text="Run Tests"
                 color="blue"
                 onClick={runTests}
+            />
+
+            <MultiSelect
+                options={tests}
+                placeholder="Select Health Checks"
+                selectedValues={selectedTests}
+                handleChange={handleMultiSelectChange}
             />
         </div>
     );
