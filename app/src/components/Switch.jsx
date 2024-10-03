2e2cb63cb21f981e3de101b2e0d23351872dac65
ryanliao296 <114436542+ryanliao296@users.noreply.github.com>
2024-10-03 17:21:56 -0400

    Updated switch to have text above and changed the color of the switch

--- app/src/components/Switch.jsx
+++ app/src/components/Switch.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
 
-const Switch = ({ isOn, handleToggle, onColor = '#06D6A0', offColor = '#ccc' }) => {
-  const switchStyles = {
-    width: '50px',
-    height: '25px',
-    borderRadius: '25px',
-    backgroundColor: isOn ? onColor : offColor,
-    position: 'relative',
-    cursor: 'pointer',
-    transition: 'background-color 0.3s',
-  };
-
-  const buttonStyles = {
-    width: '23px',
-    height: '23px',
-    backgroundColor: 'white',
-    borderRadius: '50%',
-    position: 'absolute',
-    top: '1px',
-    left: '1px',
-    transform: isOn ? 'translateX(26px)' : 'translateX(0)',
-    transition: 'transform 0.3s',
-  };
-
+const Switch = ({ isOn, handleToggle, onColor = '#06D6A0', offColor = '#ccc', onText = 'On', offText = 'Off' }) => {
   return (
-    <div style={switchStyles} onClick={handleToggle}>
-      <div style={buttonStyles} />
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <span style={{ marginBottom: '10px' }}>{isOn ? onText : offText}</span>
+
+      <div
+        className="switch"
+        onClick={handleToggle}
+        style={{
+          width: '50px',
+          height: '25px',
+          borderRadius: '25px',
+          backgroundColor: isOn ? onColor : offColor,
+          position: 'relative',
+          cursor: 'pointer',
+          transition: 'background-color 0.3s',
+        }}
+      >
+        <div
+          style={{
+            width: '23px',
+            height: '23px',
+            backgroundColor: 'white',
+            borderRadius: '50%',
+            position: 'absolute',
+            top: '1px',
+            left: '1px',
+            transform: isOn ? 'translateX(26px)' : 'translateX(0)',
+            transition: 'transform 0.3s',
+          }}
+        />
+      </div>
     </div>
   );
 };
