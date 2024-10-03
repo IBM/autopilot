f76557d8ca3ce303b500efb6163093a20b2fe801
eburhan <eburhan@bu.edu>
2024-10-02 22:55:32 -0400

    Update Monitor.jsx
    remove runTests and health checks from Autopilot services

--- app/src/Monitor.jsx
+++ app/src/Monitor.jsx
@@ -1,62 +1,28 @@
 import React, {useState, useEffect} from 'react';
 import listNodesWithStatus from "./api/getNodesWithStatus.js";
 import CollapsibleTable from "./components/CollapsibleTable.jsx";
-import runTests from "./api/runTests.js";
 
 // Displaying live node labels and status + current health checks
 
 function Monitor() {
     const [nodes, setNodes] = useState([]);
-    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const fetchHealthChecks = async () => {
-            setLoading(true);
-
-            try {
+        const fetchNodes = async () => {
+            try{
                 const nodeData = await listNodesWithStatus();
-                const healthResults = await runTests(['pciebw', 'dcgm', 'remapped', 'ping']);
-
-                // Integrate health results into node data
-                const nodesWithHealthChecks = nodeData.map(node => ({
-                    ...node,
-                    healthChecks: healthResults[node.name] || [], // Assuming health results are keyed by node name
-                }));
-
-                setNodes(nodesWithHealthChecks);
-            } catch (err) {
-                setError('Error fetching health check results');
-                console.error(err);
-            } finally {
-                setLoading(false); // Set loading to false
+                setNodes(nodeData);
+            } catch (error) {
+                console.error('Error fetching node status:', error);
+                setError('Failed to fetch node status');
             }
         };
 
-        fetchHealthChecks();
+        // Updates every 30 seconds (adjustable)
+        const intervalId = setInterval(fetchNodes, 30000);
 
-        const intervalId = setInterval(() => {
-            const updateNodeStatus = async () => {
-                try {
-                    const updatedNodeData = await listNodesWithStatus();
-                    setNodes(prevNodes => {
-                        const updatedNodesMap = {};
-                        updatedNodeData.forEach(node => {
-                            updatedNodesMap[node.name] = node;
-                        });
-
-                        // Update the existing nodes
-                        return prevNodes.map(node => updatedNodesMap[node.name] || node);
-                    });
-                } catch (err) {
-                    console.error('Error fetching node statuses:', err);
-                }
-            };
-
-            updateNodeStatus();
-        }, 10000); // update node information at every 10 seconds (adjustable)
-
-        return () => clearInterval(intervalId); // Cleaning up during unmount
+        return () => clearInterval(intervalId); // Clearing interval during unmounting component
     }, []);
 
     return (
