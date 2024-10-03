04a23a4c97546e1b16d3de61aada32cbc267638a
eburhan <eburhan@bu.edu>
2024-10-03 13:43:02 -0400

    update to work with custom hook and useSWR

--- app/src/Monitor.jsx
+++ app/src/Monitor.jsx
@@ -1,34 +1,23 @@
-import React, {useState, useEffect} from 'react';
-import listNodesWithStatus from "./api/getNodesWithStatus.js";
+import React from 'react';
+import {useNodesWithStatus} from "./api/getNodesWithStatus.js";
 import CollapsibleTable from "./components/CollapsibleTable.jsx";
 
 // Displaying live node labels and status + current health checks
 
 function Monitor() {
-    const [nodes, setNodes] = useState([]);
-    const [error, setError] = useState(null);
+    const { nodes, error } = useNodesWithStatus();
 
-    useEffect(() => {
-        const fetchNodes = async () => {
-            try{
-                const nodeData = await listNodesWithStatus();
-                setNodes(nodeData);
-            } catch (error) {
-                console.error('Error fetching node status:', error);
-                setError('Failed to fetch node status');
-            }
-        };
+    if (error) {
+        return <div>Error loading node status</div>;
+    }
 
-        // Updates every 30 seconds (adjustable)
-        const intervalId = setInterval(fetchNodes, 30000);
-
-        return () => clearInterval(intervalId); // Clearing interval during unmounting component
-    }, []);
+    if (!nodes.length) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div>
             <h1>Monitor Cluster</h1>
-            {error && <div className="error">{error}</div>}
             <CollapsibleTable nodes={nodes} />
         </div>
     );
